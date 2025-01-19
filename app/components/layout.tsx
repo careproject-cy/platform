72e10ee04ed0071e651c0375a9979c8e6feba99f
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-01-19 17:50:59 +0200

    logo

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="bg-white text-base border-b p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <Link href="/" className="text-2xl font-bold">
-            Pawsome Shelter
-          </Link>
+          <div className="flex items-center gap-3">
+            <Image src="/logo.svg" alt={'Pawsome Shelter'} width={80} height={80} />
+            <Link href="/" className="text-2xl font-bold">
+              Pawsome Shelter
+            </Link>
+          </div>
           <nav>
             <ul className="flex space-x-4">
               <li><Link href="/" className="hover:underline">Home</Link></li>
