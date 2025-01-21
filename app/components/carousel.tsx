89c5f530745435ebcdf99ffdacdb726f03e8074e
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-01-21 15:53:59 +0200

    Update carousel.tsx

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { twMerge } from "tailwind-merge";
+import { twMerge } from 'tailwind-merge';
 
 interface CarouselProps {
   images: string[];
@@ -10,8 +10,14 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ images, className }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const nextImage = () => setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  const prevImage = () => setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -20,27 +26,34 @@ const Carousel: React.FC<CarouselProps> = ({ images, className }) => {
     return () => clearInterval(interval);
   }, [images.length]);
 
+  const leftIndex = (currentIndex - 1 + images.length) % images.length;
+  const rightIndex = (currentIndex + 1) % images.length;
+  const visibleIndices = [leftIndex, currentIndex, rightIndex];
+
   return (
     <div className={twMerge(`flex flex-row items-center justify-center h-80 overflow-hidden`, className)}>
       <div className="flex flex-row items-center justify-center relative">
-        <button onClick={prevImage} className="bg-gray-100/50 p-2 rounded-full flex items-center justify-center h-8 w-8 cursor-pointer absolute left-2 z-20">
+        <button onClick={prevImage} className="bg-gray-100/50 rounded-full flex items-center justify-center h-8 w-8 cursor-pointer absolute left-20 z-20">
           <span className="material-symbols-rounded">
             chevron_left
           </span>
         </button>
-        {images.map((image, index) => {
-          const dif = index - currentIndex
-          const c = dif === 0
-            ? 'w-80 z-1 min-h-80'
-            : dif === 1 || dif === -1 ? 'w-10 z-0 min-h-72' : 'w-0 z-0 min-h-80 '
+        {visibleIndices.map((idx) => {
+          const c = idx - currentIndex === 0
+            ? 'w-80 z-1 min-h-80 rounded-xl'
+            : 'w-20 z-0 min-h-72 opacity-50';
           return (
-            <Image src={image} key={index} alt={`Slide ${currentIndex}`}
+            <Image
+              src={images[idx]}
+              key={idx}
+              alt={`Slide ${currentIndex}`}
               width={288}
               height={288}
-              className={`${c} relative object-cover h-auto transition-all duration-500 ease-in rounded-xl`} />
+              className={`${c} relative object-cover h-auto transition-height duration-300 ease-in`}
+            />
           );
         })}
-        <button onClick={nextImage} className="bg-gray-100/50 p-2 rounded-full flex items-center justify-center h-8 w-8 cursor-pointer absolute right-2 z-20">
+        <button onClick={nextImage} className="bg-gray-100/50 rounded-full flex items-center justify-center h-8 w-8 cursor-pointer absolute right-20 z-20">
           <span className="material-symbols-rounded">
             chevron_right
           </span>
