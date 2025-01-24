f9b92308878d75497ae1217affa655bc76547758
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-01-24 21:09:21 +0200

    Update page.tsx

--- app/dogs/page.tsx
+++ app/dogs/page.tsx
@@ -6,6 +6,9 @@ import DogCard from '../components/dogCard'
 import { dogs } from '../data/dogs'
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
+import Section from "@/components/ui/section"
+import Container from "@/components/ui/container"
+import Badge from "@/components/ui/badge"
 
 export default function DogsPage() {
   const [ageFilter, setAgeFilter] = useState<string>('all')
@@ -27,43 +30,45 @@ export default function DogsPage() {
 
   return (
     <Layout>
-      <h1 className="text-4xl font-bold mb-8 text-center">Our Dogs for Adoption</h1>
+      <Section className="my-12">
+        <Container className="gap-6">
+          <div className="w-full flex justify-center">
+            <Badge>Meet the dogs</Badge>
+          </div>
+          <h1 className="text-4xl font-semibold mb-4 w-full text-center">Dogs Available For Adoption</h1>
+          <div>
+            <Label htmlFor="age-filter">Filter by Age</Label>
+            <select value={"all"} onChange={(event) => setAgeFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
+              <option className="bg-white text-gray-800 p-2" value={"all"}>All Ages</option>
+              <option className="bg-white text-gray-800 p-2" value={"young"}>Young (0-3 years)</option>
+              <option className="bg-white text-gray-800 p-2" value={"adult"}>Adult (4-8 years)</option>
+              <option className="bg-white text-gray-800 p-2" value={"senior"}>Senior (9+ years)</option>
+            </select>
+          </div>
 
-      <div className="mb-8 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <div>
-          <Label htmlFor="age-filter">Filter by Age</Label>
-          <select value={"all"} onChange={(event) => setAgeFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
-            <option className="bg-white text-gray-800 p-2" value={"all"}>All Ages</option>
-            <option className="bg-white text-gray-800 p-2" value={"young"}>Young (0-3 years)</option>
-            <option className="bg-white text-gray-800 p-2" value={"adult"}>Adult (4-8 years)</option>
-            <option className="bg-white text-gray-800 p-2" value={"senior"}>Senior (9+ years)</option>
-          </select>
-        </div>
-
-        <div>
-          <Label htmlFor="breed-filter">Filter by Breed</Label>
-          <select value={"all"} onChange={(event) => setBreedFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
-            <option className="bg-white text-gray-800 p-2" value={"all"}>All Breeds</option>
-            {uniqueBreeds.map(breed => (
-              <option className="bg-white text-gray-800 p-2" key={breed} value={breed.toLowerCase()}>{breed}</option>
+          <div>
+            <Label htmlFor="breed-filter">Filter by Breed</Label>
+            <select value={"all"} onChange={(event) => setBreedFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
+              <option className="bg-white text-gray-800 p-2" value={"all"}>All Breeds</option>
+              {uniqueBreeds.map(breed => (
+                <option className="bg-white text-gray-800 p-2" key={breed} value={breed.toLowerCase()}>{breed}</option>
+              ))}
+            </select>
+          </div>
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-7">
+            {displayedDogs.map((dog) => (
+              <DogCard key={dog.id} {...dog} />
             ))}
-          </select>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-        {displayedDogs.map((dog) => (
-          <DogCard key={dog.id} {...dog} />
-        ))}
-      </div>
-
-      {visibleDogs < filteredDogs.length && (
-        <div className="text-center">
-          <Button onClick={() => setVisibleDogs(prev => prev + 20)}>
-            Load More
-          </Button>
-        </div>
-      )}
+          </div>
+          {visibleDogs < filteredDogs.length && (
+            <div className="text-center">
+              <Button onClick={() => setVisibleDogs(prev => prev + 20)}>
+                Load More
+              </Button>
+            </div>
+          )}
+        </Container>
+      </Section>
     </Layout>
   )
 }
