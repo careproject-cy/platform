27e7c18dc479ac293bb68c1840c8bc6c18104548
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-01-27 18:28:01 +0200

    Update page.tsx

--- app/dogs/page.tsx
+++ app/dogs/page.tsx
@@ -36,24 +36,25 @@ export default function DogsPage() {
             <Badge>Meet the dogs</Badge>
           </div>
           <h1 className="text-4xl font-semibold mb-4 w-full text-center">Dogs Available For Adoption</h1>
-          <div>
-            <Label htmlFor="age-filter">Filter by Age</Label>
-            <select value={"all"} onChange={(event) => setAgeFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
-              <option className="bg-white text-gray-800 p-2" value={"all"}>All Ages</option>
-              <option className="bg-white text-gray-800 p-2" value={"young"}>Young (0-3 years)</option>
-              <option className="bg-white text-gray-800 p-2" value={"adult"}>Adult (4-8 years)</option>
-              <option className="bg-white text-gray-800 p-2" value={"senior"}>Senior (9+ years)</option>
-            </select>
-          </div>
-
-          <div>
-            <Label htmlFor="breed-filter">Filter by Breed</Label>
-            <select value={"all"} onChange={(event) => setBreedFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
-              <option className="bg-white text-gray-800 p-2" value={"all"}>All Breeds</option>
-              {uniqueBreeds.map(breed => (
-                <option className="bg-white text-gray-800 p-2" key={breed} value={breed.toLowerCase()}>{breed}</option>
-              ))}
-            </select>
+          <div className="w-full flex flex-row justify-center gap-4">
+            <div className="flex flex-row justify-center items-center gap-2">
+              <Label htmlFor="age-filter">Filter by Age:</Label>
+              <select value={"all"} onChange={(event) => setAgeFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
+                <option className="bg-white text-gray-800 p-2" value={"all"}>All Ages</option>
+                <option className="bg-white text-gray-800 p-2" value={"young"}>Young (0-3 years)</option>
+                <option className="bg-white text-gray-800 p-2" value={"adult"}>Adult (4-8 years)</option>
+                <option className="bg-white text-gray-800 p-2" value={"senior"}>Senior (9+ years)</option>
+              </select>
+            </div>
+            <div className="flex flex-row justify-center items-center gap-2">
+              <Label htmlFor="breed-filter">Filter by Breed:</Label>
+              <select value={"all"} onChange={(event) => setBreedFilter(event.target.value as string)} className="p-2 border rounded-lg cursor-pointer hover:bg-gray-50">
+                <option className="bg-white text-gray-800 p-2" value={"all"}>All Breeds</option>
+                {uniqueBreeds.map(breed => (
+                  <option className="bg-white text-gray-800 p-2" key={breed} value={breed.toLowerCase()}>{breed}</option>
+                ))}
+              </select>
+            </div>
           </div>
           <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-7">
             {displayedDogs.map((dog) => (
