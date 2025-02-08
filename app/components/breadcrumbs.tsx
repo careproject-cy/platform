5dbe70f884c9c00471cbcf50ce923829dbf70743
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-02-08 20:02:28 +0200

    dog page

---
+++ app/components/breadcrumbs.tsx
@@ -0,0 +1,22 @@
+import { Row } from "@/components/ui/layout"
+import Link from 'next/link'
+import React from 'react'
+
+interface BreadcrumbsProps {
+  breadcrumbs: { href: string, text: string }[]
+}
+
+export default function Breadcrumbs({ breadcrumbs }: BreadcrumbsProps) {
+  return (
+    <Row className="w-full gap-3">
+      {breadcrumbs.map(({ href, text }, idx) => (
+        <React.Fragment key={idx}>
+          <Link href={href} className="text-lg text-gray-700 hover:text-gray-900">
+            {text}
+          </Link>
+          {idx < breadcrumbs.length - 1 && <span className="text-gray-500">/</span>}
+        </React.Fragment>
+      ))}
+    </Row>
+  )
+}
--- app/dogs/[id]/page.tsx
+++ app/dogs/[id]/page.tsx
@@ -1,13 +1,20 @@
 import { notFound } from 'next/navigation'
 import Layout from '../../components/layout'
 import { dogs } from '../../data/dogs'
+import type { Metadata, ResolvingMetadata } from 'next'
 import { Col, Row, Section, Container } from "@/components/ui/layout"
 import { PageTitle, Text, TextTitle } from "@/components/ui/typography"
 import { Divider } from "@/components/ui/divider"
 import Gallery from "@/app/components/gallery"
+import Breadcrumbs from "@/app/components/breadcrumbs"
+import { platform_name } from "@/app/data/consts"
 
 interface DogPageProps {
   params: Promise<{ id: string }>
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return { title: `${platform_name} | Dogs` }
 }
 
 export default async function DogPage({ params }: DogPageProps) {
@@ -18,23 +25,26 @@ export default async function DogPage({ params }: DogPageProps) {
     notFound()
   }
 
+  const sizeText = dog.size === "small" ? "Small (< 10 kg) size" : dog.size === "medium" ? "Medium (10-25 kg) size" : "Large (> 25kg) size"
+
   return (
     <Layout>
-      <Section className="my-10 flex-1">
-        <Container className="gap-10 flex-1 max-w-6xl">
+      <Section className="my-6 flex-1">
+        <Container className="gap-6 flex-1 py-0 max-w-6xl">
+          <Breadcrumbs breadcrumbs={[{ href: "/", text: "Home" }, { href: "/dogs", text: "Dogs" }, { href: `/dogs/${dog.id}`, text: dog.name }]} />
           <Row className="gap-6 w-full items-start md:max-lg:flex-col">
             <Gallery images={dog.images} />
             <Col className="w-full gap-6 py-6">
-              <Col className="gap-4">
+              <Col className="gap-3">
                 <Row className="w-full justify-between items-center gap-6">
                   <PageTitle>{dog.name}</PageTitle>
-                  <div className="text-md px-3 py-2 rounded-lg bg-gray-100 font-semibold text-gray-600">{dog.gender}</div>
+                  <div className="text-md px-3 py-1 rounded-lg bg-gray-100 font-semibold text-gray-600">{dog.gender}</div>
                 </Row>
-                <Col className="gap-2">
+                <Row className="w-full justify-between items-center gap-6">
                   <Text>{dog.breed}</Text>
                   <Text>~{dog.age} years old</Text>
-                  <Text>{dog.size}</Text>
-                </Col>
+                </Row>
+                <Text className="italic">{sizeText}</Text>
               </Col>
               <Divider />
               <Col className="gap-2">
