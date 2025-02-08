1ced84ac673cecff0c1f9972ef4f32e6c3ccf0c9
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-02-08 20:33:33 +0200

    Update page.tsx

--- app/dogs/[id]/page.tsx
+++ app/dogs/[id]/page.tsx
@@ -1,20 +1,32 @@
 import { notFound } from 'next/navigation'
 import Layout from '../../components/layout'
 import { dogs } from '../../data/dogs'
-import type { Metadata, ResolvingMetadata } from 'next'
-import { Col, Row, Section, Container } from "@/components/ui/layout"
-import { PageTitle, Text, TextTitle } from "@/components/ui/typography"
+import type { Metadata } from 'next'
+import { Col, Row, Section, Container, Grid4 } from "@/components/ui/layout"
+import { PageTitle, SectionTitle, Text, TextTitle } from "@/components/ui/typography"
 import { Divider } from "@/components/ui/divider"
 import Gallery from "@/app/components/gallery"
 import Breadcrumbs from "@/app/components/breadcrumbs"
 import { platform_name } from "@/app/data/consts"
+import DogCard from "@/app/components/dogCard"
+
+interface GenerateMetadataProps {
+  params: { id: string }
+}
+
+export async function generateMetadata({ params }: GenerateMetadataProps): Promise<Metadata> {
+  const { id } = await params
+  const dog = dogs.find((d) => d.id === id)
+  if (!dog) {
+    return { title: `${platform_name} | Not found` }
+  }
+  return {
+    title: `${platform_name} | ${dog.name} | ${dog.breed}`
+  }
+}
 
 interface DogPageProps {
   params: Promise<{ id: string }>
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  return { title: `${platform_name} | Dogs` }
 }
 
 export default async function DogPage({ params }: DogPageProps) {
@@ -26,11 +38,12 @@ export default async function DogPage({ params }: DogPageProps) {
   }
 
   const sizeText = dog.size === "small" ? "Small (< 10 kg) size" : dog.size === "medium" ? "Medium (10-25 kg) size" : "Large (> 25kg) size"
+  const similarDogs = dogs.filter(d => d.size === dog.size && d.id !== dog.id).slice(0, 4)
 
   return (
     <Layout>
-      <Section className="my-6 flex-1">
-        <Container className="gap-6 flex-1 py-0 max-w-6xl">
+      <Section className="my-10 flex-1">
+        <Container className="gap-10 flex-1 py-0 max-w-6xl">
           <Breadcrumbs breadcrumbs={[{ href: "/", text: "Home" }, { href: "/dogs", text: "Dogs" }, { href: `/dogs/${dog.id}`, text: dog.name }]} />
           <Row className="gap-6 w-full items-start md:max-lg:flex-col">
             <Gallery images={dog.images} />
@@ -65,6 +78,13 @@ export default async function DogPage({ params }: DogPageProps) {
               </Col>
             </Col>
           </Row>
+          <Divider />
+          <SectionTitle className="w-full">Similar Dogs</SectionTitle>
+          <Grid4>
+            {similarDogs.map((dog) => (
+              <DogCard key={dog.id} {...dog} />
+            ))}
+          </Grid4>
         </Container>
       </Section>
     </Layout>
