f85ce9d773c23330bd7b423f4cb09c48d8dcb618
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-02-22 21:53:49 +0200

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,5 +18,5 @@ export const Button: React.FC<TypographyComponentProps> = (props) =>
       lg: "shadow-md hover:shadow-lg",
       xl: "shadow-lg hover:shadow-xl",
     })
-    .withTypography()
+    .withTypography({})
     .build();
