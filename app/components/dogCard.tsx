25e1ba1be86be2f255b0fd25fa4f89b7667ef627
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-02-25 14:44:54 +0200

    chip added

--- app/components/dogCard.tsx
+++ app/components/dogCard.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { getImageSrc } from "../utils/images"
 import { DogMetadata } from "../data/dogMetadata"
+import { Chip } from "@/components/ui/chip"
 
 export default function DogCard(dog: DogMetadata) {
   const dogImages = dog.images.map((image) => getImageSrc(image));
@@ -27,13 +28,13 @@ export default function DogCard(dog: DogMetadata) {
               className="relative object-cover h-auto w-full aspect-square"
             />
             {showStatus &&
-              <div className="text-sm px-2 py-1 rounded-lg bg-gray-100/75 absolute right-2 top-2 text-gray-700/75 font-semibold">{status}</div>
+              <Chip semibold absolute sans secondary className="right-2 bottom-2 opacity-75">{status}</Chip>
             }
           </Col>
           <Col xs>
             <Row vCentered justifyBetween>
               <TextTitle>{name}</TextTitle>
-              <div className="text-sm px-2 py-1 rounded-lg bg-gray-100 font-semibold text-gray-600">{gender}</div>
+              <Chip semibold sm sans>{gender}</Chip>
             </Row>
             <Text secondary italic>{breed}</Text>
             <Text secondary sm>{age} years old</Text>
