534aa72917990bbb6415588353d87c344420adbd
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-06-15 19:21:57 +0300

    Update page.tsx

--- app/dogs/[location]/[file]/page.tsx
+++ app/dogs/[location]/[file]/page.tsx
@@ -30,9 +30,11 @@ export async function generateMetadata({ params }: IdProps): Promise<Metadata> {
     };
   }
   const imgUrl = getImageSrc(dog.images[0]);
+  const url = `https://${domain}/dogs/${location}/${file}`
   return {
     title: `${dog.name} | ${dog.breed} | ${platform_name}`,
     openGraph: {
+      url: url,
       images: [
         {
           url: imgUrl || "care-project-social.png",
