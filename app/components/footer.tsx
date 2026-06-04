5a460adf61fadafefbaa70b59bc2d466938ce278
Evgeniy Kosjakov <elv1s42k@gmail.com>
2026-06-04 17:48:58 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Container, Link as VLink, Section, Text, Col, Row, Title, Card } from "@vaneui/ui"
+import { Container, Link as VLink, Section, Text, Col, Row, Title, Card, IconButton } from "@vaneui/ui"
 import { platform_name } from "../data/consts"
 import Link from 'next/link'
 import Image from "next/image";
@@ -15,7 +15,7 @@ export default function Footer() {
           <Col lg className="max-w-lg">
             <Link href="/">
               <Row itemsCenter>
-                <Image src="/logo.png" alt={platform_name} width={80} height={80} className="h-12 w-auto rounded-lg"/>
+                <Image src="/logo.png" alt={platform_name} width={80} height={80} className="h-12 w-auto rounded-lg" />
                 <Title lg serif bold mobileHide className="text-white">
                   {platform_name}
                 </Title>
@@ -24,19 +24,16 @@ export default function Footer() {
             <Row>
               {
                 [
-                  {icon: Instagram, link: "https://www.instagram.com/uana.cy/"},
-                  {icon: Facebook, link: "https://www.facebook.com/careproject.cy"},
-                  {icon: Linkedin, link: "https://www.linkedin.com/company/uana-foundation/"},
-                  {icon: Send, link: "https://t.me/care_project"},
-                  {icon: GitHub, link: "https://github.com/careproject-cy/"},
+                  { icon: Instagram, link: "https://www.instagram.com/uana.cy/" },
+                  { icon: Facebook, link: "https://www.facebook.com/careproject.cy" },
+                  { icon: Linkedin, link: "https://www.linkedin.com/company/uana-foundation/" },
+                  { icon: Send, link: "https://t.me/care_project" },
+                  { icon: GitHub, link: "https://github.com/careproject-cy/" },
                 ].map((item, i) => (
-                    <Link href={item.link} key={i}>
-                      <Card xs primary noBorder filled wFit className="opacity-75 hover:opacity-100 duration-300 bg-bg-filled-hover-primary">
-                        <item.icon className="text-text-warning size-7"/>
-                      </Card>
-                    </Link>
-                  )
-                )
+                  <IconButton secondary lg filled href={item.link} key={i}>
+                    <item.icon />
+                  </IconButton>
+                ))
               }
             </Row>
             <Text lg className="text-white">
@@ -52,31 +49,31 @@ export default function Footer() {
                 {
                   category: "About",
                   links: [
-                    {href: "/", text: "Home"},
-                    {href: "/dogs", text: "Dogs"},
-                    {href: "/blog", text: "Blog"},
-                    {href: "/more/about", text: "About"},
+                    { href: "/", text: "Home" },
+                    { href: "/dogs", text: "Dogs" },
+                    { href: "/blog", text: "Blog" },
+                    { href: "/more/about", text: "About" },
                   ]
                 },
                 {
                   category: "Support us",
                   links: [
-                    {href: "/more/get-involved", text: "Get Involved"},
-                    {href: "/more/adopt", text: "Adopt us"},
-                    {href: "/more/donate", text: "Donate"},
-                    {href: "/more/foster", text: "Foster"},
+                    { href: "/more/get-involved", text: "Get Involved" },
+                    { href: "/more/adopt", text: "Adopt us" },
+                    { href: "/more/donate", text: "Donate" },
+                    { href: "/more/foster", text: "Foster" },
                   ]
                 }
               ].map((item, index) => (
-                  <Col lg key={index} className="w-64">
-                    <Text xl semibold uppercase className="text-white opacity-50 pt-3">{item.category}</Text>
-                    <Col sm>
-                      {item.links.map((link, i) => (
-                        <VLink noUnderline tag={Link} key={i} href={link.href} className="text-white">{link.text}</VLink>
-                      ))}
-                    </Col>
+                <Col lg key={index} className="w-64">
+                  <Text xl semibold uppercase className="text-white opacity-50 pt-3">{item.category}</Text>
+                  <Col sm>
+                    {item.links.map((link, i) => (
+                      <VLink noUnderline tag={Link} key={i} href={link.href} className="text-white">{link.text}</VLink>
+                    ))}
                   </Col>
-                )
+                </Col>
+              )
               )
             }
           </Row>
